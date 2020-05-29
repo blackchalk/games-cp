90125777b40a0999324a345042cc137880235169
Alvin L. Raygon <alvinraygon@yahoo.com>
2020-05-29 16:24:23 +0800

    reinit .gitignore to add .ds_store

---
+++ practice-problems/codeforces/problem_677a.cpp
@@ -0,0 +1,15 @@
+//http://codeforces.com/contest/677/problem/A
+#include<iostream>
+using namespace std;
+int main(){
+    int n,h,a,min_roadwidth = 0;
+    cin>>n>>h;
+    while (n--)
+    {
+        cin>>a;
+        if(a>h){min_roadwidth+=2;}
+        else{min_roadwidth+=1;}
+    }
+    cout << min_roadwidth;
+    return 0;
+}
