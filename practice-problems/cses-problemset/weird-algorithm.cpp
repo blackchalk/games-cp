e9788bb0813c1ba2111747d19103def53b8354e4
Alvin L. Raygon <alvinraygon@yahoo.com>
2020-05-29 10:59:44 +0800

    reinit .gitignore to include dot vscode settings folder

--- practice-problems/cses-problemset/weird-algorithm.cpp
+++ practice-problems/cses-problemset/weird-algorithm.cpp
@@ -8,8 +8,8 @@ int main(){
  cin >> n;
 	while(true){
 	cout << n << " ";
-	if(n==1){break;}
-	if(n%2 == 0){ n = n/2;}
-	else{ n = (n * 3) + 1;}
+	if(n==1)break;
+	if(n%2 == 0) n = n/2;
+	else n = (n * 3) + 1;
 }
 }
