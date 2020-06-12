52b1f1e70206a74675253db02836203b1d12db9b
Alvin L. Raygon <alvinraygon@yahoo.com>
2020-06-12 15:35:32 +0800

    A. Word

---
+++ practice-problems/codeforces/problem59a.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+string s;
+cin >> s;
+int l=0,c=0;
+for (int i = 0; i < s.size(); i++)
+{
+    if (s[i] < 92)
+    {
+        c++;
+    }else{
+        l++;
+    }
+}
+
+if (c>l)
+{
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] > 92)
+        {
+            s[i] -= 32;
+        }
+    }
+}else{
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] < 92)
+        {
+            s[i] += 32;
+        }
+    }
+}
+
+cout << s;
+return 0;
+}
