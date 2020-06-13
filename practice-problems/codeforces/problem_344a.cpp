7f937b3cccf3f819d6ef56e1bde25664dc2ba452
Alvin L. Raygon <alvinraygon@yahoo.com>
2020-06-13 20:49:37 +0800

    A. Magnets

---
+++ practice-problems/codeforces/problem_344a.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+int m, count=0, ans=1,cnt=0;
+char c[1000000];
+cin >> m;
+while (count < (m*2))
+{
+    cin >> c[count] >> c[count+1];
+    count = count + 2;
+}
+while (cnt < strlen(c)-1)
+{
+    if(c[cnt] == c[cnt+1]){
+        ans++;
+    }
+    cnt++;
+}
+cout << ans << "\n";
+return 0;
+}
